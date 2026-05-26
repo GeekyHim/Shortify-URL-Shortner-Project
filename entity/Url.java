06bbb13c1d0c5baa6be874b8e9872a7d3b23bf9b
Himanshu Gupta <work.himanshu1720@gmail.com>
2026-05-26 16:40:39 +0530

    initial commit

---
+++ entity/Url.java
@@ -0,0 +1,34 @@
+package com.geekyhim.shortify.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String shortCode;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiryDate;
+
+    private Long clickCount;
+
+    private LocalDateTime lastAccessedAt;
+}
